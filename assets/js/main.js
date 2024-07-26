370a56f4140779f85c00a42ae8f3132a68fc8a59
Hasnain khan <hasnaink1480@gmail.com>
2024-07-26 16:08:53 +0500

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,12 +68,46 @@ emailjs.sendForm('service_kqfoodj','template_x3z1erp','#contact-form','dxeGyiEiL
 contactForm.addEventListener('submit',sentEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr=ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:2500,
+    delay:400,
+    // reset:true 
+})
+sr.reveal(`.home--profile, .about--image, .contact--mail`, {origin:'right'})
+sr.reveal(`.home--name, .home--info,.about--container,.contact--data,.contact--social,.section--title-1,.about--info`, {origin:'left'})
+sr.reveal(`.services--card, .projects--card`,{interval:100})
